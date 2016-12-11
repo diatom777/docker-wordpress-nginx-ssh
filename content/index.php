0be2d1f15630de1cf5b0c591e75692eb86339002
diatom777 <leksoid@gmail.com>
2016-12-11 17:02:01 +0200

    Update index.php

--- content/index.php
+++ content/index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World Latvia and Canada</p>'; ?> 
+ <?php echo '<p>Hello World Latvia and Canada! Haliluya!</p>'; ?> 
  </body>
 </html>
