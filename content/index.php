59ccaa91f8c6d3402519cf161a53e2417f812d21
diatom777 <leksoid@gmail.com>
2016-12-12 19:18:40 +0200

    Update index.php

--- content/index.php
+++ content/index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World Latvia and Canada! Haliluya! Maybe yes/maybe no. Works? Hell yeah! Comon! Final test!</p>'; ?> 
+ <?php echo '<p>Hello yeah!!!!!!!!!!!!!!!</p>'; ?> 
  </body>
 </html>
